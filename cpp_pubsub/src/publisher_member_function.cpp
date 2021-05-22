59b9f047a03496e7603221b948197ba11eafe5ff
Zhe-Ming, Zhang <48936051+qaz9517532846@users.noreply.github.com>
2021-05-22 13:53:57 +0800

    Add files via upload

--- cpp_pubsub/src/publisher_member_function.cpp
+++ cpp_pubsub/src/publisher_member_function.cpp
@@ -4,7 +4,7 @@
 #include <string>
 
 #include "rclcpp/rclcpp.hpp"
-#include "std_msgs/msg/string.hpp"
+#include "cpp_srvcli/msg/string.hpp"
 
 using namespace std::chrono_literals;
 
@@ -14,7 +14,7 @@ class MinimalPublisher : public rclcpp::Node
     MinimalPublisher()
     : Node("minimal_publisher"), count_(0)
     {
-      publisher_ = this->create_publisher<std_msgs::msg::String>("topic", 10);
+      publisher_ = this->create_publisher<cpp_srvcli::msg::String>("topic", 10);
       timer_ = this->create_wall_timer(
       500ms, std::bind(&MinimalPublisher::timer_callback, this));
     }
@@ -22,13 +22,13 @@ class MinimalPublisher : public rclcpp::Node
   private:
     void timer_callback()
     {
-      auto message = std_msgs::msg::String();
-      message.data = "Hello, world! " + std::to_string(count_++);
-      RCLCPP_INFO(this->get_logger(), "Publishing: '%s'", message.data.c_str());
+      auto message = cpp_srvcli::msg::String();
+      message.world = "Hello, world! " + std::to_string(count_++);
+      RCLCPP_INFO(this->get_logger(), "Publishing: '%s'", message.world.c_str());
       publisher_->publish(message);
     }
     rclcpp::TimerBase::SharedPtr timer_;
-    rclcpp::Publisher<std_msgs::msg::String>::SharedPtr publisher_;
+    rclcpp::Publisher<cpp_srvcli::msg::String>::SharedPtr publisher_;
     size_t count_;
 };
 
--- cpp_pubsub/src/subscriber_member_function.cpp
+++ cpp_pubsub/src/subscriber_member_function.cpp
@@ -1,7 +1,7 @@
 #include <memory>
 
 #include "rclcpp/rclcpp.hpp"
-#include "std_msgs/msg/string.hpp"
+#include "cpp_srvcli/msg/string.hpp"
 using std::placeholders::_1;
 
 class MinimalSubscriber : public rclcpp::Node
@@ -10,16 +10,16 @@ class MinimalSubscriber : public rclcpp::Node
     MinimalSubscriber()
     : Node("minimal_subscriber")
     {
-      subscription_ = this->create_subscription<std_msgs::msg::String>(
+      subscription_ = this->create_subscription<cpp_srvcli::msg::String>(
       "topic", 10, std::bind(&MinimalSubscriber::topic_callback, this, _1));
     }
 
   private:
-    void topic_callback(const std_msgs::msg::String::SharedPtr msg) const
+    void topic_callback(const cpp_srvcli::msg::String::SharedPtr msg) const
     {
-      RCLCPP_INFO(this->get_logger(), "I heard: '%s'", msg->data.c_str());
+      RCLCPP_INFO(this->get_logger(), "I heard: '%s'", msg->world.c_str());
     }
-    rclcpp::Subscription<std_msgs::msg::String>::SharedPtr subscription_;
+    rclcpp::Subscription<cpp_srvcli::msg::String>::SharedPtr subscription_;
 };
 
 int main(int argc, char * argv[])
