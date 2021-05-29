ac74ed1fb7f408869a7d0ec6d5644a6f804c9d32
qaz9517532846 <qaz9517532846@gmail.com>
2021-05-29 10:40:47 +0800

    updated

---
+++ py_pubsub/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup
+
+package_name = 'py_pubsub'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='zmtech',
+    maintainer_email='zmtech@todo.todo',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+                'talker = py_pubsub.publisher_member_function:main',
+                'listener = py_pubsub.subscriber_member_function:main',
+                'server = py_pubsub.server_member_function:main',
+                'client = py_pubsub.client_member_function:main',
+        ],
+    },
+)
